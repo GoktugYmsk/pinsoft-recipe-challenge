9922fefc9ef8f5823545a9e6e3cfb165d5cafc99
GoktugYmsk <gogtug033@outlook.com>
2024-01-24 12:49:05 +0300

    edited addRecipe menu

--- src/components/addRecipe/index.jsx
+++ src/components/addRecipe/index.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 import Header from '../header'
 import './index.scss'
 
 function AddRecipe() {
+    const [category, setCategory] = useState('Tatlı');
+    const navigate = useNavigate();
+
+
+    const handleReturnMainPage = () => {
+        navigate('/')
+    };
+
     return (
         <>
             <Header />
-            <div>
-                AddRecipe
+            <div className='container-addRecipe' >
+                <div className='container-addRecipe__box' >
+                    <input />
+                    <textarea />
+                    <select
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    >
+                        <option value='' disabled>Kategori</option>
+                        <option value='Eletronik'>Tatlı</option>
+                        <option value='Giyim'>Çorba</option>
+                        <option value='Kitap'>Ana Yemek</option>
+                    </select>
+                    <button>Tarifi ekle</button>
+                    <button onClick={handleReturnMainPage} >Ana Menüye dön</button>
+                </div>
             </div>
         </>
     )
--- src/components/hamburgerMenu/index.jsx
+++ src/components/hamburgerMenu/index.jsx
@@ -36,6 +36,7 @@ function HamburgerMenu() {
 
 
     const handleAddClick = () => {
+        dispatch(setIsHamburger(false));
         navigate('/addRecipe');
     };
 
