eee5f6303d6144176d28118834d3129d03539d1f
GoktugYmsk <gogtug033@outlook.com>
2024-01-24 11:31:45 +0300

    header page edited

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -3,10 +3,13 @@ import { FaRegComment } from 'react-icons/fa';
 import { CiEdit } from 'react-icons/ci';
 import { MdOutlineAdd } from 'react-icons/md';
 import { FaStar } from 'react-icons/fa';
+import { MdDelete } from "react-icons/md";
+import Dropdown from 'react-bootstrap/Dropdown';
 import './index.scss';
 
 function Content() {
     const [rating, setRating] = useState(0);
+    const [isAdmin, setIsAdmin] = useState(false);
 
     const handleStarHover = (hoveredStar) => {
         if (rating === 0) {
@@ -30,6 +33,17 @@ function Content() {
             <div className='leftContent' >
                 <div className='topContent__filter'>
                     <input />
+                    <Dropdown  >
+                        <Dropdown.Toggle className='topContent__filter__dropdown'>
+                            Kategori
+                        </Dropdown.Toggle>
+
+                        <Dropdown.Menu className='topContent__filter_-dropdownMenu' >
+                            <Dropdown.Item className='topContent__filter_-dropdownMenu__hover' href="#/action-1">Tatlılar</Dropdown.Item>
+                            <Dropdown.Item className='topContent__filter_-dropdownMenu__hover' href="#/action-2">Çorbalar</Dropdown.Item>
+                            <Dropdown.Item className='topContent__filter_-dropdownMenu__hover' href="#/action-3">Ana Yemekler</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                 </div>
             </div>
             <div className='altContent' >
@@ -42,6 +56,9 @@ function Content() {
                             <FaRegComment className='icon' />
                             <CiEdit className='icon' />
                             <MdOutlineAdd className='icon' />
+                            {isAdmin &&
+                                <MdDelete className='deleteIcon' />
+                            }
                             <div className='point'>
                                 {[1, 2, 3, 4, 5].map((star) => (
                                     <FaStar
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import './index.scss'
-import { IoIosLogOut } from "react-icons/io";
+import React, { useEffect, useState } from 'react';
+import './index.scss';
+import { IoIosLogOut } from 'react-icons/io';
+import { FaRegUser } from 'react-icons/fa';
 
 function Header() {
+    const [islogin, setIslogin] = useState(false);
+
     return (
         <>
             <div className='container-header'>
                 <h1>PİNSOFT RECİPE</h1>
-                <IoIosLogOut className='container-header__logoutIcon' />
+                {!islogin ? (
+                    <div className='container-header__login' >
+                        <FaRegUser className='container-header__loginIcon' />
+                        <h5>Göktuğ</h5>
+                    </div>
+                ) : (
+                    <IoIosLogOut className='container-header__logoutIcon' />
+                )}
             </div>
         </>
-    )
+    );
 }
 
-export default Header
+export default Header;
