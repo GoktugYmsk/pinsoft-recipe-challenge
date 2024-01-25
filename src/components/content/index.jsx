d67dab19c03708132482e0eeff0cce7436271029
GoktugYmsk <gogtug033@outlook.com>
2024-01-25 22:30:48 +0300

    added filter function

--- src/components/content/index.jsx
+++ src/components/content/index.jsx
@@ -15,10 +15,31 @@ function Content() {
     const [recipe, setRecipe] = useState('');
     const [recipePopup, setRecipePopup] = useState(false);
     const [isCommenting, setIsCommenting] = useState(false);
+    const [searchText, setSearchText] = useState('');
+    const [selectedCategory, setSelectedCategory] = useState('');
 
     const textareRef = useRef();
 
-    console.log('recipe', recipe);
+    const recipes = [
+        { name: 'Yemek adı 1', category: 'Tatlılar', content: 'Lorem ipsum dolor sit amet...' },
+        { name: 'Yemek adı 2', category: 'Çorbalar', content: 'Lorem ipsum dolor sit amet...' },
+        // Diğer yemekler
+    ];
+
+    const filterRecipes = () => {
+        // Filtreleme işlemleri burada yapılır
+        let filteredRecipes = recipes;
+
+        if (searchText) {
+            filteredRecipes = filteredRecipes.filter(recipe => recipe.name.toLowerCase().includes(searchText.toLowerCase()));
+        }
+
+        if (selectedCategory) {
+            filteredRecipes = filteredRecipes.filter(recipe => recipe.category === selectedCategory);
+        }
+
+        return filteredRecipes;
+    };
 
     const handleStarHover = (hoveredStar) => {
         if (rating === 0) {
@@ -27,7 +48,6 @@ function Content() {
     };
 
     const handleStarClick = (clickedStar) => {
-        console.log('deneme', clickedStar)
         setRating(clickedStar);
     };
 
@@ -41,13 +61,6 @@ function Content() {
         setRecipePopup(true);
     };
 
-    useEffect(() => {
-        if (isCommenting === true) {
-
-        }
-    }, [isCommenting])
-
-
     const handleCommentClick = () => {
         if (textareRef.current) {
             textareRef.current.blur();
@@ -57,139 +70,81 @@ function Content() {
     return (
         <>
             <div className='container-content'>
-                <div className='leftContent' >
+                <div className='leftContent'>
                     <div className='topContent__filter'>
-                        <input />
-                        <Dropdown  >
+                        <input onChange={(e) => setSearchText(e.target.value)} value={searchText} />
+                        <Dropdown>
                             <Dropdown.Toggle className='topContent__filter__dropdown'>
                                 Kategori
                             </Dropdown.Toggle>
-                            <Dropdown.Menu className='topContent__filter_-dropdownMenu' >
-                                <Dropdown.Item className='topContent__filter_-dropdownMenu__hover' href="#/action-1">Tatlılar</Dropdown.Item>
-                                <Dropdown.Item className='topContent__filter_-dropdownMenu__hover' href="#/action-2">Çorbalar</Dropdown.Item>
-                                <Dropdown.Item className='topContent__filter_-dropdownMenu__hover' href="#/action-3">Ana Yemekler</Dropdown.Item>
+                            <Dropdown.Menu className='topContent__filter_-dropdownMenu'>
+                                <Dropdown.Item
+                                    className='topContent__filter_-dropdownMenu__hover'
+                                    onClick={() => setSelectedCategory('Tatlılar')}
+                                >
+                                    Tatlılar
+                                </Dropdown.Item>
+                                <Dropdown.Item
+                                    className='topContent__filter_-dropdownMenu__hover'
+                                    onClick={() => setSelectedCategory('Çorbalar')}
+                                >
+                                    Çorbalar
+                                </Dropdown.Item>
+                                <Dropdown.Item
+                                    className='topContent__filter_-dropdownMenu__hover'
+                                    onClick={() => setSelectedCategory('Ana Yemekler')}
+                                >
+                                    Ana Yemekler
+                                </Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
                     </div>
                 </div>
-                <div className='altContent' >
-                    <div className='container-content__recipe'>
-                        <h2>Yemek adı</h2>
-                        <div className='container-content__recipe__altBox'>
-                            <img src='https://cdn.yemek.com/mnresize/940/940/uploads/2023/10/saray-koftesi-yemekcom.jpg' />
+                <div className='altContent'>
+                    {filterRecipes().map((filteredRecipe, index) => (
+                        <div key={index} className='container-content__recipe'>
+                            <h2>{filteredRecipe.name}</h2>
+                            <div className='container-content__recipe__altBox'>
+                                <img src='https://cdn.yemek.com/mnresize/940/940/uploads/2023/10/saray-koftesi-yemekcom.jpg' />
 
-                            <div className='container-content__recipe__altBox__icons'>
-                                <FaRegComment onClick={handleCommentClick} className='icon' />
-                                <CiEdit onClick={handleEditClick} className='icon' />
-                                <MdOutlineAdd className='icon' />
-                                {isAdmin &&
-                                    <MdDelete className='deleteIcon' />
-                                }
-                                <div className='point'>
-                                    {[1, 2, 3, 4, 5].map((star) => (
-                                        <FaStar
-                                            key={star}
-                                            className={`icon ${star <= rating ? 'ratingYellow' : 'ratingGrey'}`}
-                                            onMouseEnter={() => handleStarHover(star)}
-                                            onMouseLeave={handleStarLeave}
-                                            onClick={() => handleStarClick(star)}
-                                        />
-                                    ))}
+                                <div className='container-content__recipe__altBox__icons'>
+                                    <FaRegComment
+                                        onClick={handleCommentClick}
+                                        className='icon'
+                                    />
+                                    <CiEdit
+                                        onClick={handleEditClick}
+                                        className='icon'
+                                    />
+                                    <MdOutlineAdd className='icon' />
+                                    {isAdmin &&
+                                        <MdDelete className='deleteIcon' />
+                                    }
+                                    <div className='point'>
+                                        {[1, 2, 3, 4, 5].map((star) => (
+                                            <FaStar
+                                                key={star}
+                                                className={`icon ${star <= rating ? 'ratingYellow' : 'ratingGrey'}`}
+                                                onMouseEnter={() => handleStarHover(star)}
+                                                onMouseLeave={handleStarLeave}
+                                                onClick={() => handleStarClick(star)}
+                                            />
+                                        ))}
+                                    </div>
                                 </div>
+                                <textarea ref={textareRef} />
+                                <p>{filteredRecipe.content}</p>
                             </div>
-                            <textarea ref={textareRef} />
                         </div>
-                        <p>{recipe}</p>
-                    </div>
-                    <div className='container-content__recipe'>
-                        <h2>Yemek adı</h2>
-                        <div className='container-content__recipe__altBox'>
-                            <img src='https://cdn.yemek.com/mnresize/940/940/uploads/2023/10/saray-koftesi-yemekcom.jpg' />
-                            <div className='container-content__recipe__altBox__icons'>
-                                <FaRegComment className='icon' />
-                                <CiEdit className='icon' />
-                                <MdOutlineAdd className='icon' />
-                                <div className='point'>
-                                    {[1, 2, 3, 4, 5].map((star) => (
-                                        <FaStar
-                                            key={star}
-                                            className={`icon ${star <= rating ? 'ratingYellow' : 'ratingWhite'}`}
-                                            onMouseEnter={() => handleStarHover(star)}
-                                            onMouseLeave={handleStarLeave}
-                                            onClick={() => handleStarClick(star)}
-                                        />
-                                    ))}
-                                </div>
-                            </div>
-                            <textarea />
-                        </div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                        </p>
-                    </div>
-                    <div className='container-content__recipe'>
-                        <h2>Yemek adı</h2>
-                        <div className='container-content__recipe__altBox'>
-                            <img src='https://cdn.yemek.com/mnresize/940/940/uploads/2023/10/saray-koftesi-yemekcom.jpg' />
-                            <div className='container-content__recipe__altBox__icons'>
-                                <FaRegComment className='icon' />
-                                <CiEdit className='icon' />
-                                <MdOutlineAdd className='icon' />
-                                <div className='point'>
-                                    {[1, 2, 3, 4, 5].map((star) => (
-                                        <FaStar
-                                            key={star}
-                                            className={`icon ${star <= rating ? 'ratingYellow' : 'ratingGrey'}`}
-                                            onMouseEnter={() => handleStarHover(star)}
-                                            onMouseLeave={handleStarLeave}
-                                            onClick={() => handleStarClick(star)}
-                                        />
-                                    ))}
-                                </div>
-                            </div>
-                            <textarea />
-                        </div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                        </p>
-                    </div>
-                    <div className='container-content__recipe'>
-                        <h2>Yemek adı</h2>
-                        <div className='container-content__recipe__altBox'>
-                            <img src='https://cdn.yemek.com/mnresize/940/940/uploads/2023/10/saray-koftesi-yemekcom.jpg' />
-                            <div className='container-content__recipe__altBox__icons'>
-                                <FaRegComment className='icon' />
-                                <CiEdit className='icon' />
-                                <MdOutlineAdd className='icon' />
-                                <div className='point'>
-                                    {[1, 2, 3, 4, 5].map((star) => (
-                                        <FaStar
-                                            key={star}
-                                            className={`icon ${star <= rating ? 'ratingYellow' : 'ratingGrey'}`}
-                                            onMouseEnter={() => handleStarHover(star)}
-                                            onMouseLeave={handleStarLeave}
-                                            onClick={() => handleStarClick(star)}
-                                        />
-                                    ))}
-                                </div>
-                            </div>
-                            <textarea />
-                        </div>
-                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum veniam deserunt sapiente nesciunt sunt minus autem excepturi sint iusto, ut officiis eveniet laborum repellendus maxime illum animi error amet eum?
-                        </p>
-                    </div>
+                    ))}
                 </div>
             </div>
             {recipePopup &&
-                <Modal className='products-popup-container' show={true} onHide={() => setRecipePopup(false)} >
+                <Modal className='products-popup-container' show={true} onHide={() => setRecipePopup(false)}>
                     <Modal.Header closeButton>
                         <Modal.Title>Edit Recipe</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body className='products-popup-container__box' >
+                    <Modal.Body className='products-popup-container__box'>
                         <textarea onChange={(e) => setRecipe(e.target.value)} />
                     </Modal.Body>
                     <Modal.Footer>
