aa2ada052c6e9e94f08540da5c219e279ad3f070
GoktugYmsk <gogtug033@outlook.com>
2024-01-27 10:22:11 +0300

    added addRecipe function

--- src/components/addRecipe/index.jsx
+++ src/components/addRecipe/index.jsx
@@ -5,9 +5,13 @@ import Header from '../header'
 import HamburgerMenu from '../hamburgerMenu';
 import Button from 'react-bootstrap/Button';
 import './index.scss'
+import api from '../../interceptor';
 
 function AddRecipe() {
     const [category, setCategory] = useState('Tatlı');
+    const [recipeName, setRecipeName] = useState('');
+    const [prepare, setPrepare] = useState('');
+    const [ingredients, setIngredients] = useState('');
     const navigate = useNavigate();
 
     const isHamburger = useSelector((state) => state.recipeBooleanControl.isHamburger);
@@ -21,6 +25,30 @@ function AddRecipe() {
         navigate('/')
     };
 
+    const handleAddRecipe = () => {
+        const fetchData = async () => {
+            try {
+                const newRecipe = {
+                    id: 0,
+                    name: recipeName,
+                    explanation: prepare,
+                    category: {
+                        id: 1,
+                        name: category
+                    }
+                };
+                const sendRecipe = await api.post('/recipe', newRecipe);
+                console.log('Tarif başarıyla eklendi:', sendRecipe);
+            } catch (error) {
+                console.error('Veri gönderilemedi:', error);
+            }
+        };
+
+        fetchData();
+    };
+
+
+
     return (
         <>
             <Header />
@@ -28,15 +56,15 @@ function AddRecipe() {
                 <div className='container-addRecipe__box' >
                     <h1>Tarif Ekle</h1>
                     <div className='container-addRecipe__box__top' >
-                        <input placeholder='Tarifin adı' />
-                        <textarea className='textare-one' placeholder='Hazırlanışı' />
-                        <textarea className='textare-two' placeholder='Malzemeler' />
+                        <input onChange={(e) => setRecipeName(e.target.value)} placeholder='Tarifin adı' />
+                        <textarea onChange={(e) => setPrepare(e.target.value)} className='textare-one' placeholder='Hazırlanışı' />
+                        <textarea onChange={(e) => setIngredients(e.target.value)} className='textare-two' placeholder='Malzemeler' />
                     </div>
                     <div className='select' >
                         <select
                             value={category}
                             onChange={(e) => setCategory(e.target.value)}>
-                            <option value='' disabled>Kategori</option>
+                            <option value='Kategori Seçiniz' disabled>Kategori</option>
                             <option value='Eletronik'>Tatlı</option>
                             <option value='Giyim'>Çorba</option>
                             <option value='Kitap'>Ana Yemek</option>
@@ -44,7 +72,7 @@ function AddRecipe() {
                     </div>
                     <div className='container-addRecipe__box__down' >
                         <Button onClick={handleReturnMainPage} >Ana Menüye dön</Button>
-                        <Button>Tarifi ekle</Button>
+                        <Button onClick={handleAddRecipe} >Tarifi ekle</Button>
                     </div>
                 </div>
             </div>
