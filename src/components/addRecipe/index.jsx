ce7731e89e57f7230f6c0d3052f36bf6d03c624b
GoktugYmsk <gogtug033@outlook.com>
2024-01-30 17:45:16 +0300

    edited handleAddRecipe function max 250 characters

--- src/components/addRecipe/index.jsx
+++ src/components/addRecipe/index.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import Header from '../header'
+import Header from '../header';
 import HamburgerMenu from '../hamburgerMenu';
 import Button from 'react-bootstrap/Button';
-import './index.scss'
+import './index.scss';
 import api from '../../interceptor';
 
 function AddRecipe() {
@@ -12,42 +12,56 @@ function AddRecipe() {
     const [recipeName, setRecipeName] = useState('');
     const [prepare, setPrepare] = useState('');
     const [ingredients, setIngredients] = useState('');
+    const [photo, setPhoto] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     const navigate = useNavigate();
 
     const isHamburger = useSelector((state) => state.recipeBooleanControl.isHamburger);
 
     useEffect(() => {
-        console.log('isHamburger', isHamburger)
+        console.log('isHamburger', isHamburger);
     }, [isHamburger]);
 
-
     const handleReturnMainPage = () => {
-        navigate('/')
+        navigate('/');
     };
 
     const handleAddRecipe = () => {
+        if (prepare.length > 250) {
+            setErrorMessage('Hazırlanışı alanına en fazla 250 karakter girebilirsiniz.');
+            return;
+        }
+
         const fetchData = async () => {
             try {
-                let categoryId;
+                let selectCategoryId;
                 switch (category) {
-                    case 'Tatlı':
-                        categoryId = 100;
-                        break;
-                    case 'Çorba':
-                        categoryId = 101;
+                    case 'Kahvaltı':
+                        selectCategoryId = 1;
                         break;
                     case 'Ana Yemek':
-                        categoryId = 102;
+                        selectCategoryId = 2;
+                        break;
+                    case 'Tatlı':
+                        selectCategoryId = 3;
+                        break;
+                    case 'Salata':
+                        selectCategoryId = 4;
                         break;
                 }
 
                 const newRecipe = {
                     name: recipeName,
                     explanation: prepare,
-                    categoryId: categoryId,
+                    categoryId: selectCategoryId,
+                    base64img: photo,
                 };
 
                 const sendRecipe = await api.post('/recipe', newRecipe);
+
+                if (sendRecipe.status === 200) {
+                    navigate('/');
+                }
                 console.log('Tarif başarıyla eklendi:', sendRecipe);
             } catch (error) {
                 console.error('Veri gönderilemedi:', error);
@@ -57,39 +71,68 @@ function AddRecipe() {
         fetchData();
     };
 
+    const handleFileChange = (e) => {
+        const file = e.target.files?.[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onloadend = () => {
+                const base64Image = reader.result;
+                setPhoto(base64Image);
+            };
+
+            reader.readAsDataURL(file);
+        }
+    };
 
     return (
         <>
             <Header />
-            <div className='container-addRecipe' >
-                <div className='container-addRecipe__box' >
+            <div className={`container-addRecipe ${isHamburger ? 'opacityActive' : ''}`}>
+                <div className='container-addRecipe__box'>
                     <h1>Tarif Ekle</h1>
-                    <div className='container-addRecipe__box__top' >
+                    <div className='container-addRecipe__box__top'>
                         <input onChange={(e) => setRecipeName(e.target.value)} placeholder='Tarifin adı' />
-                        <textarea onChange={(e) => setPrepare(e.target.value)} className='textare-one' placeholder='Hazırlanışı' />
+                        <textarea
+                            onChange={(e) => setPrepare(e.target.value)}
+                            className={`textare-one ${prepare.length > 250 ? 'error' : ''}`}
+                            placeholder='Hazırlanışı'
+                        />
+                        {prepare.length > 250 && (
+                            <p className="error-message">Hazırlanışı alanına en fazla 250 karakter girebilirsiniz.</p>
+                        )}
                         <textarea onChange={(e) => setIngredients(e.target.value)} className='textare-two' placeholder='Malzemeler' />
                     </div>
-                    <div className='select' >
+                    <div className='select'>
                         <select
                             value={category}
                             onChange={(e) => setCategory(e.target.value)}>
                             <option value='Kategori Seçiniz' disabled>Kategori</option>
-                            <option value='Eletronik'>Tatlı</option>
-                            <option value='Giyim'>Çorba</option>
-                            <option value='Kitap'>Ana Yemek</option>
+                            <option value='Kahvaltı'>Kahvaltı</option>
+                            <option value='Ana Yemek'>Ana Yemek</option>
+                            <option value='Tatlı'>Tatlı</option>
+                            <option value='Salata'>Salata</option>
                         </select>
+                        <label htmlFor='file-input'>
+                            <input
+                                id='file-input'
+                                type='file'
+                                style={{ display: 'none' }}
+                                onChange={(e) => handleFileChange(e)}
+                            />
+                            Add Photo
+                        </label>
                     </div>
-                    <div className='container-addRecipe__box__down' >
-                        <Button onClick={handleReturnMainPage} >Ana Menüye dön</Button>
-                        <Button onClick={handleAddRecipe} >Tarifi ekle</Button>
+                    <div className='container-addRecipe__box__down'>
+                        <Button onClick={handleReturnMainPage}>Ana Menüye dön</Button>
+                        <Button onClick={handleAddRecipe}>Tarifi ekle</Button>
                     </div>
                 </div>
             </div>
-            {isHamburger &&
-                <HamburgerMenu />
-            }
+            {isHamburger && <HamburgerMenu />}
         </>
-    )
+    );
 }
 
-export default AddRecipe
+export default AddRecipe;
